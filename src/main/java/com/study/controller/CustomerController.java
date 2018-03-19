3c8e3b567f06fc444e4870ed7ceaa388fcb42764
liuwy <731839038@qq.com>
2018-03-19 17:57:43 +0800

    update

--- src/main/java/com/study/controller/CustomerController.java
+++ src/main/java/com/study/controller/CustomerController.java
@@ -1,5 +1,7 @@
 package com.study.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -16,11 +18,31 @@ import com.study.service.CityDubboConsumerService;
 @RequestMapping("/consumer")
 public class CustomerController {
 
+	private Logger logger = LoggerFactory.getLogger(CustomerController.class);
+
 	@Autowired
 	private CityDubboConsumerService consumerService;
 
 	@RequestMapping(path = "/city", method = RequestMethod.GET)
 	public City getCity() {
 		return consumerService.getCityByName("luoyang");
+	}
+
+	@RequestMapping(path = "/memory/dead/loop", method = RequestMethod.GET)
+	public String testMemory() {
+		logger.debug("线程启动");
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				int i = 1;
+				while (true) {
+					logger.debug("线程启动==========> 次数:{}", i++);
+				}
+			}
+
+		}).start();
+		return "dead-loop start";
+
 	}
 }
